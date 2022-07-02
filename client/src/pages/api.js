491e4c3709d21fe1ecdeeef367602d55522af5c2
Arjun Singh <arjunkhakh@gmail.com>
2022-07-02 18:06:35 +0100

    working on the design of the featured games section

--- client/src/pages/api.js
+++ client/src/pages/api.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Apidata1 = () => {
     
@@ -15,18 +19,38 @@ const [users, setUsers] = useState();
     getApiData();
   }, []);
 
+  // const MetaReview = async (user) => {
+  //   if (user.metacritic < 0.50) {
+  //     document.getElementById("metra").setAttribute("style", "background-color:red; padding:2px; border: 1px solid black; clear:both; display: inline-block; overflow: hidden;white-space: nowrap; margin:0px")
+  //   } else if (user.metacritic < 0.75) {
+  //     document.getElementById("metra").setAttribute("style", "background-color:yellow; padding:2px; border: 1px solid black; margin:0px; clear:both; display: inline-block; overflow: hidden;white-space: nowrap;")
+  //   } else {
+  //     document.getElementById("metra").setAttribute("style", "background-color:lightgreen; padding:2px; border: 1px solid black; margin:0px; clear:both; display: inline-block; overflow: hidden;white-space: nowrap;")
+  //   }
+  // }
+
   return (
     <div>
       <h1>Featured Games</h1>
-      {users &&
-        users.results.map((user) => (
-          <div key={user.id} className="item-container">
-            Id:{user.id}<div className="title">Title:{user.name}</div>
-            <div> <img className="homeimage" alt="the video game" src={user.background_image}></img></div>
-          </div>
-        ))}
-    </div>
+        <Container>
+          <Row>
+          {users &&
+         users.results.map((user) => (
+              <Col key={user.id} xs="4">
+                <Box>
+                <div className="title">{user.name}</div>
+              <div> <img className="homeimage" alt="the video game" src={user.background_image}></img></div>
+              {/* <MetaReview> <div id="metra">{user.metacritic}</div> </MetaReview> */}
+              
+              </Box>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
   );
 }
+
+const Box = props => <div className="box">{props.children}</div>;
 
 export default Apidata1
